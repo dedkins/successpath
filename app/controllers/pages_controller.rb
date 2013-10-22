3cd77a583a85008a332f7d09f42d0fcc9b4f6b67
David Edkins <dedkins@edkinsgroup.com>
2013-10-21 17:52:50 -0700

    updated some css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   def home
   	if member_signed_in?
   		@member = Member.find(current_member)
-  		@opentasks = Task.where("member_id = ? and achievedate is ? and duedate <= ?", current_member, nil, Date.today).order("duedate ASC").limit(4)
-  		@goals_next30 = Goal.where("member_id = ? and achievedate is ?", current_member, nil).limit(4)
+  		@opentasks = Task.where("member_id = ? and achievedate is ? and duedate <= ?", current_member, nil, Date.today).order("duedate ASC").limit(3)
+  		@goals_next30 = Goal.where("member_id = ? and achievedate is ?", current_member, nil).limit(3).order("duedate ASC")
       @goals = Goal.find_all_by_member_id(current_member)
       @affirmations = Affirmation.find_all_by_member_id(current_member)
   	end
