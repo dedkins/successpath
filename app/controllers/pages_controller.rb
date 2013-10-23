f1a1d9994825da83623c9ea6b0f83a900597f4ab
David Edkins <dedkins@edkinsgroup.com>
2013-10-23 08:41:42 -0700

    updated got to edit after signup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
   def home
   	if member_signed_in?
   		@member = Member.find(current_member)
+      if @member.mainlifetitle == nil
+        redirect_to edit_member_registration_path(@member), :notice => "Welcome Aboard! You are now free to change your life! :)"
+      end
   		@opentasks = Task.where("member_id = ? and achievedate is ? and duedate <= ?", current_member, nil, Date.today).order("duedate ASC").limit(3)
   		@goals_next30 = Goal.where("member_id = ? and achievedate is ?", current_member, nil).limit(3).order("goaldate ASC")
       @goals = Goal.find_all_by_member_id(current_member)
