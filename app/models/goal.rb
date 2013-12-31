57d9c4e65223078a7ad3e1a5376f1d63102cbef3
David Edkins <dedkins@edkinsgroup.com>
2013-12-31 10:37:34 -0800

    added destroy method to goal task model

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,7 +2,7 @@ class Goal < ActiveRecord::Base
   attr_accessible :description, :goaldate, :image, :name, :priority, :url, :user_id, :achievedate, :progress
 
   belongs_to :member
-  has_many :tasks, dependent => :destroy
+  has_many :tasks, :dependent => :destroy
 
   accepts_nested_attributes_for :tasks, :member
   
